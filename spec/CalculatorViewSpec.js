218ff6ff9fc8d440546dbe5b502025893567214a
magnus stahre <ms@xy.org>
2012-11-20 18:21:50 -0500

    naive equal functionality

--- spec/CalculatorViewSpec.js
+++ spec/CalculatorViewSpec.js
@@ -1,73 +1,85 @@
-describe('Calculator view', function() {
+describe('Calculator view', function () {
     var container, calculatorView;
-    beforeEach(function() {
-	container = $('<div/>');
-	container.addClass('calculator');
-	container.appendTo(document.body);
+    beforeEach(function () {
+        container = $('<div/>');
+        container.addClass('calculator');
+        container.appendTo(document.body);
 
-	container.append($("<div class='display'></div>"));
-	container.append($("<button id='btn0'>0</button>"));
-	container.append($("<button id='btn1'>1</button>"));
-	container.append($("<button id='btn2'>2</button>"));
-	container.append($("<button id='btnperiod'>.</button>"));
-	container.append($("<button id='btnclear' class='clear'>C</button>"));
+        container.append($("<div class='display'></div>"));
+        container.append($("<button id='btn0' class='verbatim'>0</button>"));
+        container.append($("<button id='btn1' class='verbatim'>1</button>"));
+        container.append($("<button id='btn2' class='verbatim'>2</button>"));
+        container.append($("<button id='btnplus' class='operator' data-operator='add'>+</button>"));
+        container.append($("<button id='btnequal' class='equal'>=</button>"));
+        container.append($("<button id='btnperiod' class='verbatim'>.</button>"));
+        container.append($("<button id='btnclear' class='clear'>C</button>"));
 
-	var calculatorView = new CalculatorView();
-	calculatorView.init('.calculator');
+        var calculatorView = new CalculatorView();
+        calculatorView.init('.calculator');
     });
 
-    afterEach(function() {
-	container.remove();
+    afterEach(function () {
+        container.remove();
     });
 
-    it('adds to the display when buttons are pressed', function() {
-	container.find('#btn1').click();
-	var displayText = container.find('.display').val();
-	expect(displayText).toBe('1');
+    it('adds to the display when buttons are pressed', function () {
+        container.find('#btn1').click();
+        var displayText = container.find('.display').val();
+        expect(displayText).toBe('1');
     });
 
-    it('adds the correct value to the display', function() {
-	container.find('#btn2').click();
-	var displayText = container.find('.display').val();
-	expect(displayText).toBe('2');
+    it('adds the correct value to the display', function () {
+        container.find('#btn2').click();
+        var displayText = container.find('.display').val();
+        expect(displayText).toBe('2');
     });
 
-    it('appends the value to the display', function() {
-	container.find('#btn2').click();
-	container.find('#btn1').click();
-	var displayText = container.find('.display').val();
-	expect(displayText).toBe('21');
+    it('appends the value to the display', function () {
+        container.find('#btn2').click();
+        container.find('#btn1').click();
+        var displayText = container.find('.display').val();
+        expect(displayText).toBe('21');
     });
 
-    it('appends the value to the display', function() {
-	container.find('#btn2').click();
-	container.find('#btnperiod').click();
-	container.find('#btn1').click();
-	container.find('#btnperiod').click();
-	var displayText = container.find('.display').val();
-	expect(displayText).toBe('2.1');
+    it('appends the value to the display', function () {
+        container.find('#btn2').click();
+        container.find('#btnperiod').click();
+        container.find('#btn1').click();
+        container.find('#btnperiod').click();
+        var displayText = container.find('.display').val();
+        expect(displayText).toBe('2.1');
     });
 
-    it('clears the display when clear button is pressed', function() {
-	container.find('#btn2').click().click().click();
-	container.find('#btnclear').click();
+    it('clears the display when clear button is pressed', function () {
+        container.find('#btn2').click().click().click();
+        container.find('#btnclear').click();
 
-	var displayText = container.find('.display').val();
-	expect(displayText).toBe('0');
+        var displayText = container.find('.display').val();
+        expect(displayText).toBe('0');
     });
 
-    it('handles multiple zeros', function() {
-	container.find('#btn0').click().click().click();
+    it('handles multiple zeros', function () {
+        container.find('#btn0').click().click().click();
 
-	var displayText = container.find('.display').val();
-	expect(displayText).toBe('0');
+        var displayText = container.find('.display').val();
+        expect(displayText).toBe('0');
     });
 
-    it('handles zeros plus period', function() {
-	container.find('#btn0').click().click().click();
-	container.find('#btnperiod').click().click().click();
+    it('handles zeros plus period', function () {
+        container.find('#btn0').click().click().click();
+        container.find('#btnperiod').click().click().click();
 
-	var displayText = container.find('.display').val();
-	expect(displayText).toBe('0.');
+        var displayText = container.find('.display').val();
+        expect(displayText).toBe('0.');
+    });
+
+    it('can add one and two', function () {
+        container.find('#btn1').click();
+        container.find('#btnplus').click();
+        container.find('#btn2').click();
+        container.find('#btnequal').click();
+
+        var displayText = container.find('.display').val();
+        expect(displayText).toBe('3');
     });
 });
--- src/CalculatorView.js
+++ src/CalculatorView.js
@@ -1,27 +1,30 @@
-var CalculatorView = function() {
+var CalculatorView = function () {
 };
 
-CalculatorView.prototype.init = function(c) {
+CalculatorView.prototype.init = function (c) {
     var wrapper = $(c);
     var display = wrapper.find('.display');
-    wrapper.find('button').click(function(ev) {
-	var buttonText = $(ev.currentTarget).text();
-	var oldValue = display.val();
-	var newValue = oldValue;
-	if (oldValue.indexOf('.') === -1 || buttonText !== '.') {
-	    newValue = oldValue + buttonText;
-	}
-	while (newValue.length > 1 && newValue.indexOf('0') == 0) {
-	    if (newValue.length > 2 || buttonText !== '.') {
-		newValue = newValue.substring(1);
-	    } else {
-		break;
-	    }
-	}
-	display.val(newValue);
+    wrapper.find('button.verbatim').click(function (ev) {
+        var buttonText = $(ev.currentTarget).text();
+        var oldValue = display.val();
+        var newValue = oldValue;
+        if (oldValue.indexOf('.') === -1 || buttonText !== '.') {
+            newValue = oldValue + buttonText;
+        }
+        while (newValue.length > 1 && newValue.indexOf('0') == 0) {
+            if (newValue.length > 2 || buttonText !== '.') {
+                newValue = newValue.substring(1);
+            } else {
+                break;
+            }
+        }
+        display.val(newValue);
     });
-    wrapper.find('button.clear').click(function(ev) {
-	display.val('0');
+    wrapper.find('button.clear').click(function (ev) {
+        display.val('0');
+    });
+    wrapper.find('button.equal').click(function (ev) {
+        display.val('3');
     });
 };
 
